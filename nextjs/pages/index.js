029e9ce016d289697ff476a58dcf8a592bd3e0e4
askibinski <albert.skibinski@intracto.com>
2021-02-24 11:51:47 +0100

    apollo query

--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -1,17 +1,32 @@
 import Head from 'next/head'
+// Import the component from our local storybook package.
 import { TextAndImage } from 'storybook-demo'
+// https://www.apollographql.com/docs/react/get-started/
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { gql } from '@apollo/client';
 
 export default function Home() {
 
-  // @todo get this from graphql.
-  const props = {
-    text: 'Lorem ipsum',
-    image: {
-      src: 'https://source.unsplash.com/random',
-      alt: 'Random picture from unsplash',
-      width: 600,
-    },
-  };
+  const client = new ApolloClient({
+    uri: 'http://drupal9.lndo.site/graphql',
+    cache: new InMemoryCache()
+  });
+
+  client.query({
+    query: gql`
+
+    `
+  })
+  .then(result => console.log(result));
+
+  // const props = {
+  //   text: 'Lorem ipsum',
+  //   image: {
+  //     src: 'https://source.unsplash.com/random',
+  //     alt: 'Random picture from unsplash',
+  //     width: 600,
+  //   },
+  // };
 
   return (
     <div className="container">
